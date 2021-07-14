8af384e1d61500a6752a88e926747a8cc722fbdc
LeoIIAtreides <87129661+LeoIIAtreides@users.noreply.github.com>
2021-07-14 18:22:47 +0100

    Massive overhaul with Nina - Now working in VS CODE, new environ, fixed PATH issues

---
+++ Code_Pipeline/LK_Masters_Data_Pipeline/Test Module1.py
@@ -0,0 +1,25 @@
+# %%
+from Secrets import TOKEN
+from twarc import Twarc2, expansions
+import json
+import ssl
+client = Twarc2(bearer_token=TOKEN)
+
+randTweets = []
+
+def main():
+    for count, result in enumerate(client.sample()):
+        tweet = expansions.flatten(result)
+        randTweets.append(tweet)
+       # print(json.dumps(tweet))
+        if count > 3:
+            break
+
+    with open('data.json', 'w') as f:
+        json.dump(randTweets,f)
+
+
+
+if __name__ == "__main__":
+    main()
+# %%
