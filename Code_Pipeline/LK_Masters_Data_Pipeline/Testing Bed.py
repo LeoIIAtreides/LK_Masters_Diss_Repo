68ea2358032e95756f1f841b17f691b94e2882b0
LeoIIAtreides <87129661+LeoIIAtreides@users.noreply.github.com>
2021-07-19 21:17:01 +0100

    Create Testing Bed.py
    test bed doc for applying lessons on python basics, noting examples

---
+++ Code_Pipeline/LK_Masters_Data_Pipeline/Testing Bed.py
@@ -0,0 +1,51 @@
+#name = input("What is your Name?: ")
+#age = input("What is your Age?: ")
+#print("Hello "+name+", age "+str(age))
+#import math
+#pi = 3.14
+#print(round(pi))
+#rounds to nearest whole number. ceil rounds high, floor rounds low, abs (absolute value) shows distance from zero (+/-), pow raises a base number to a power (e.g. pow(pi, 2))
+#print(math.floor(pi))
+
+#char_name = "Tom"
+#char_age = 45.5
+#is_male = True
+#print("There once was a man named " + char_name + ".")
+#print("He was " + str(char_age) + " years old.")
+
+#phrase = "Screw You"
+#print(phrase.upper())
+#print(phrase.index("Y"))
+#print(phrase.replace('You', 'Me'))
+
+#import math
+#print(math.sqrt(36))
+
+#name = "Leo Knowles"
+
+#first_name = name[0:4]
+#last_name = name[4:11]
+#print(first_name)
+#print(last_name)
+
+#website = "http://google.com"
+#slice = slice(7,-4)
+#print(website[slice])
+
+#age = int(input("How old are you?"))
+
+#if age >= 18:
+    #print("you are an adult.")
+#elif age < 0:
+    print("you aren't alive, buddy")
+#else:
+    #print("you are under-age")
+
+
+temp=int(input("what is the temp outside?: "))
+if temp >= 0 and temp <= 30:
+    print("pretty nice")
+elif temp > 30 or temp <0:
+    print("not so good")
+
+#issues with intend??
